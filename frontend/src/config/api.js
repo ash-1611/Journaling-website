b6d185da1ae8e97e83a8a563fb03510fb8d86a4d
Aishwarya Das <aishwaryadas1611@gmail.com>
2026-04-22 21:08:44 +0530

    fix

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,6 @@
-const API_BASE = process.env.REACT_APP_API_URL;
+const rawBase = process.env.REACT_APP_API_URL || "";
+
+// Ensure we don't generate URLs like: https://host.com//api/...
+const API_BASE = rawBase.replace(/\/+$/, "");
 
 export default API_BASE;
